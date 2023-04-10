80871fe3c22ea0f99682228005394596634b3f65
Lorenz Gerber <lorenz.gerber@lablicate.com>
2023-04-10 17:29:15 +0200

    p 120, CommonReducer added

--- src/shop/CategoryNavigation.js
+++ src/shop/CategoryNavigation.js
@@ -1,12 +1,13 @@
 import React, { Component } from "react";
-import { Link } from "react-router-dom";
+// import { Link } from "react-router-dom";
+import { ToggleLink } from "../ToggleLink";
 
 export class CategoryNavigation extends Component {
     
     render(){
         return <React.Fragment>
-            <Link className="btn btn-secondary form-control m-1" to={ this.props.baseUrl }>All</Link>
-            { this.props.categories && this.props.categories.map(cat => <Link className="btn btn-secondary form-control m-1" key={ cat } to={ `${this.props.baseUrl}/${cat.toLowerCase()}`}> { cat }</Link>)}
+            <ToggleLink to={ this.props.baseUrl } exact={ true }>All</ToggleLink>
+            { this.props.categories && this.props.categories.map(cat => <ToggleLink key={ cat } to={ `${this.props.baseUrl}/${cat.toLowerCase()}`}> { cat }</ToggleLink>)}
         </React.Fragment>
     }
 }
