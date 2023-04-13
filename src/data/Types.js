962812cae8367308e573f3e0a04672b0dd5b74e6
Lorenz Gerber <lorenz.gerber@lablicate.com>
2023-04-13 14:28:16 +0200

    pass down props addToCart

--- src/data/Types.js
+++ src/data/Types.js
@@ -1,4 +1,6 @@
-export const DataTypes = { PRODUCTS: "products", CATEGORIES: "categories" }
+export const DataTypes = { 
+    PRODUCTS: "products", 
+    CATEGORIES: "categories" }
 
 export const ActionTypes = { 
     DATA_LOAD: "data_load",
--- src/shop/Shop.js
+++ src/shop/Shop.js
@@ -17,7 +17,7 @@ export class Shop extends Component {
                     <CategoryNavigation baseUrl="/shop/products" categories={ this.props.categories } />
                 </div>
                 <div className="col-6 p-2">
-                    <ProductList products={ this.props.products } />
+                    <ProductList products={ this.props.products } addToCart={ this.props.addToCart } />
                 </div>
             </div>
         </div>
