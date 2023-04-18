e57699f9db16af9372bda228cc515027adfb8a1f
Lorenz Gerber <lorenz.gerber@lablicate.com>
2023-04-18 11:39:56 +0200

    using rest data source p135 works

--- src/data/DataStore.js
+++ src/data/DataStore.js
@@ -1,7 +1,8 @@
 
-import { legacy_createStore as createStore} from 'redux'
+import { legacy_createStore as createStore, applyMiddleware } from 'redux';
 import { ShopReducer } from "./ShopReducer";
 import { CartReducer } from "./CartReducer";
 import { CommonReducer } from "./CommonReducer";
+import { asyncActions } from "./AsyncMiddleware";
 
-export const SportsStoreDataStore = createStore(CommonReducer(ShopReducer, CartReducer));
+export const SportsStoreDataStore = createStore(CommonReducer(ShopReducer, CartReducer), applyMiddleware(asyncActions));
