f20f4bc7431c8c0fc2f9117d61524784339d8346
Lorenz Gerber <lorenz.gerber@lablicate.com>
2023-04-24 22:00:21 +0200

    new data source p.166

--- data.js
+++ data.js
@@ -1,10 +1,10 @@
 var faker = require('faker');
-var data = [];
 var categories = ["Watersports", "Soccer", "Chess", "Running"];
+var products = [];
 faker.seed(100);
 for(let i = 1; i <= 503; i++){
     var category = faker.helpers.randomize(categories);
-    data.push({
+    products.push({
         id: i,
         name: faker.commerce.productName(),
         category: category,
@@ -13,10 +13,30 @@ for(let i = 1; i <= 503; i++){
     })
 }
 
-module.exports = function() {
-    return {
-        categories: categories,
-        products: data,
-        orders: []
+var orders = [];
+for (let i = 1; i <= 103; i++ ){
+    var fname = faker.name.firstName(); var sname = faker.name.lastName();
+    var order = {
+        id: i, name: `${fname} ${sname}`,
+        email: faker.internet.email(fname, sname),
+        address: faker.address.zipCode(), country: faker.address.country(),
+        shipped: faker.random.boolean(), products: []
     }
+    var productCount = faker.random.number({min: 1, max: 5});
+    var product_ids = [];
+    while (product_ids.length < productCount){
+        var candidateId = faker.random.number({ min: 1, max: products.length});
+        if (product_ids.indexOf(candidateId) === -1){
+            product_ids.push(candidateId);
+        }
+    }
+    for (let j = 0; j < productCount; j++ ){
+        order.products.push({
+            quantity: faker.random.number({min: 1, max: 10}),
+                product_id: product_ids[j]
+        })
+    }
+    orders.push(order);
 }
+
+module.exports = () => ({ categories, products, orders })
